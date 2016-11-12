c30759317ad82d6219f7093b80994da546fdb830
Aukai Tirrell <mtirrell@hawaii.edu>
2016-11-12 12:37:44 -1000

    passed test

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,16 @@
 'use strict';
 module.exports = function() {
-  
+
+
+  var rand = Math.floor((Math.random() * 1000000) + 1);
+
+  function newNum( ) {
+    return rand;
+  }
+  return newNum;
+
+
 };
+
+
+
