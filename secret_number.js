44e7a80998772331db8ccc12c4918bf56910b914
Aukai Tirrell <mtirrell@hawaii.edu>
2016-11-12 13:38:42 -1000

    changed newNum function to just return

--- secret_number.js
+++ secret_number.js
@@ -4,10 +4,9 @@ module.exports = function() {
 
   var rand = Math.floor((Math.random() * 1000000) + 1);
 
-  function newNum( ) {
+  return function newNum( ) {
     return rand;
-  }
-  return newNum;
+  };
 
 
 };
